2fd5561f9a789e707b80cfe78922014f627fe2b9
sonke <inwoker123@mail.ru>
2022-11-15 19:03:22 +0300

    change in test and gradle

--- src/test/java/ru/netology/ApplicationCardTest.java
+++ src/test/java/ru/netology/ApplicationCardTest.java
@@ -1,41 +1,17 @@
 package ru.netology;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import io.github.bonigarcia.wdm.WebDriverManager;
-
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 
-
 public class ApplicationCardTest {
-    WebDriver driver;
-    private static ChromeOptions options;
-
-    @BeforeAll
-    static void setUpAll() {
-        options = new ChromeOptions();
-        options.addArguments("--headless");
-        WebDriverManager.chromedriver().setup();
-    }
 
     @BeforeEach
     void setUp() {
-        driver = new ChromeDriver(options);
         open("http://localhost:9999/");
-    }
-
-    @AfterEach
-    void tearDown() {
-        driver.quit();
-        driver = null;
     }
 
     @Test
@@ -58,7 +34,7 @@ public class ApplicationCardTest {
     }
 
     @Test
-    public void testSendFormWithZero() {
+    public void testPhoneNumberConsistsZero() {
         $("[data-test-id=name] input").setValue("Миронов Артур");
         $("[data-test-id=phone] input").setValue("+70000000000");
         $("[data-test-id=agreement]").click();
